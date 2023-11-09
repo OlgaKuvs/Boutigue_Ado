7d04bfe2aed6aaf796b5aa52f3dc9c06f86146a7
OlgaKuvs <olja_kuv@inbox.lv>
2023-11-09 17:39:57 +0000

    finished product size logic

--- bag/views.py
+++ bag/views.py
@@ -13,14 +13,28 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        size = request.POST['product_size']
     bag = request.session.get('bag', {})
-    # print('BAG1', bag)
+    print('BAG1', bag)
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if size:
+        if item_id in list(bag.keys()):
+            print("bag.key", list(bag.keys()))
+            print("bag.value", list(bag.values()))
+            print("bag[item_id]['items_by_size'].keys() = ", bag[item_id]['items_by_size'].keys())
+            if size in bag[item_id]['items_by_size'].keys():
+                bag[item_id]['items_by_size'][size] += quantity
+            else:
+                bag[item_id]['items_by_size'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_size': {size: quantity}}
     else:
-        bag[item_id] = quantity
-    # print('BAG2', bag[item_id])
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     # print(request.session['bag'])
