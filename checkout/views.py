85860bc23082fcab60a099333147668da707b96a
OlgaKuvs <olja_kuv@inbox.lv>
2023-11-13 16:46:35 +0000

    added checkout form data caching in payment intent

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,22 @@ from bag.contexts import bag_contents
 import stripe
 import json
 
+def cache_checkout_data(request):
+    try:
+        pid = request.POST.get('client_secret').split('_secret')[0]
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        stripe.PaymentIntent.modify(pid, metadata={
+            'bag': json.dumps(request.session.get('bag', {})),
+            'save_info': request.POST.get('save_info'),
+            'username': request.user,
+        })
+        return HttpResponse(status=200)
+    except Exception as e:
+        messages.error(request, ('Sorry, your payment cannot be '
+                                 'processed right now. Please try '
+                                 'again later.'))
+        return HttpResponse(content=e, status=400)
+
 
 def checkout(request):
     stripe_public_key = settings.STRIPE_PUBLIC_KEY
@@ -38,10 +54,10 @@ def checkout(request):
         order_form = OrderForm(form_data)
         if order_form.is_valid():
             order = order_form.save()
-            # pid = request.POST.get('client_secret').split('_secret')[0]
-            # order.stripe_pid = pid
-            # order.original_bag = json.dumps(bag)
-            # order.save()
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)
@@ -153,13 +169,7 @@ def checkout_success(request, order_number):
 
     return render(request, template, context)
 
-
-       
-
     
-
-
-
 # @require_POST
 # def cache_checkout_data(request):
 #     try:
