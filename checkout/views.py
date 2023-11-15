3ddf4d7e5d56a89a0990ba23cb5879a7d492ec9e
OlgaKuvs <olja_kuv@inbox.lv>
2023-11-15 19:06:53 +0000

    added products form

--- checkout/views.py
+++ checkout/views.py
@@ -4,6 +4,8 @@ from django.shortcuts import (
 from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
 
 from .forms import OrderForm
 from .models import Order, OrderLineItem
@@ -146,6 +148,24 @@ def checkout(request):
     return render(request, template, context)
 
 
+# def send_confirmation_email(order):
+#     """Send the user a confirmation email"""
+#     cust_email = order.email
+#     subject = render_to_string(
+#         'checkout/confirmation_emails/confirmation_email_subject.txt',
+#         {'order': order})
+#     body = render_to_string(
+#         'checkout/confirmation_emails/confirmation_email_body.txt',
+#         {'order': order, 'contact_email': settings.DEFAULT_FROM_EMAIL})
+    
+#     send_mail(
+#         subject,
+#         body,
+#         settings.DEFAULT_FROM_EMAIL,
+#         [cust_email]
+#     )
+
+
 def checkout_success(request, order_number):
     """
     Handle successful checkouts
@@ -177,6 +197,8 @@ def checkout_success(request, order_number):
     messages.success(request, f'Order successfully processed! \
         Your order number is {order_number}. A confirmation \
         email will be sent to {order.email}.')
+    
+    # send_confirmation_email(order)
 
     if 'bag' in request.session:
         del request.session['bag']
